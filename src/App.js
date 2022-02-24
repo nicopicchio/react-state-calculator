2f07999e8c2134832e62dd10aa4dd7c952954a33
Nico Picchio <nicolapicchio@gmail.com>
2022-02-24 15:08:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ function App() {
         <div className="panel">
           <p>0</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
             <button>7</button>
             <button>8</button>
             <button>9</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
             <button>0</button>
+            <button>.</button>
             <button>Clear</button>
           </div>
         </div>
@@ -34,16 +35,17 @@ function App() {
         <div className="panel">
           <p>0</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
             <button>7</button>
             <button>8</button>
             <button>9</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
             <button>0</button>
+            <button>.</button>
             <button>Clear</button>
           </div>
         </div>
